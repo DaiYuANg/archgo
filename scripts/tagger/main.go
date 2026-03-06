a7575d181c963ad0a29e64a862d01df2ad81d65f
ddddd <dai_yuang@icloud.com>
2026-03-06 12:14:12 +0800

    chore(release): harden tagger flow and add CI check workflow

--- scripts/tagger/main.go
+++ scripts/tagger/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"regexp"
-	"sort"
 	"strconv"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
@@ -13,64 +14,62 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
-func main() {
+var semverTag = regexp.MustCompile(`^v(\d+)\.(\d+)\.(\d+)$`)
 
-	// 打开当前仓库
+type version struct {
+	major int
+	minor int
+	patch int
+}
+
+func (v version) String() string {
+	return fmt.Sprintf("v%d.%d.%d", v.major, v.minor, v.patch)
+}
+
+func (v version) Less(other version) bool {
+	if v.major != other.major {
+		return v.major < other.major
+	}
+	if v.minor != other.minor {
+		return v.minor < other.minor
+	}
+	return v.patch < other.patch
+}
+
+type bumpMode int
+
+const (
+	bumpPatch bumpMode = iota
+	bumpMinor
+	bumpMajor
+)
+
+func main() {
+	push := flag.Bool("push", false, "push the created tag to remote")
+	remote := flag.String("remote", "origin", "remote used when --push is enabled")
+	minor := flag.Bool("minor", false, "bump minor version")
+	major := flag.Bool("major", false, "bump major version")
+	patch := flag.Bool("patch", false, "bump patch version (default)")
+	taggerName := flag.String("tagger-name", "auto-tagger", "tagger name")
+	taggerEmail := flag.String("tagger-email", "ci@local", "tagger email")
+	flag.Parse()
+
+	mode := resolveBumpMode(*major, *minor, *patch)
+
 	repo, err := git.PlainOpen(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 获取所有 tag
-	tagIter, err := repo.Tags()
+	latest, err := latestSemverTag(repo)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var tags []string
-
-	err = tagIter.ForEach(func(ref *plumbing.Reference) error {
-		tags = append(tags, ref.Name().Short())
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	re := regexp.MustCompile(`v(\d+)\.(\d+)\.(\d+)`)
-
-	// 过滤符合 semver 的 tag
-	var versions []string
-	for _, t := range tags {
-		if re.MatchString(t) {
-			versions = append(versions, t)
-		}
-	}
-
-	if len(versions) == 0 {
-		versions = append(versions, "v0.0.0")
-	}
-
-	// 排序找到最大版本
-	sort.Slice(versions, func(i, j int) bool {
-		return versions[i] < versions[j]
-	})
-
-	latest := versions[len(versions)-1]
-
-	match := re.FindStringSubmatch(latest)
-
-	major, _ := strconv.Atoi(match[1])
-	minor, _ := strconv.Atoi(match[2])
-	patch, _ := strconv.Atoi(match[3])
-
-	patch++
-
-	newTag := fmt.Sprintf("v%d.%d.%d", major, minor, patch)
-
+	next := bump(latest, mode)
+	newTag := next.String()
 	fmt.Println("New tag:", newTag)
 
-	// 获取 HEAD commit
 	head, err := repo.Head()
 	if err != nil {
 		log.Fatal(err)
@@ -81,11 +80,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// 创建 annotated tag
 	_, err = repo.CreateTag(newTag, commit.Hash, &git.CreateTagOptions{
 		Tagger: &object.Signature{
-			Name:  "auto-tagger",
-			Email: "ci@local",
+			Name:  *taggerName,
+			Email: *taggerEmail,
+			When:  time.Now(),
 		},
 		Message: newTag,
 	})
@@ -93,18 +92,114 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Tag created")
+	fmt.Println("Tag created locally")
 
-	// push tag
+	if !*push {
+		fmt.Printf("Push manually: git push %s %s\n", *remote, newTag)
+		return
+	}
+
 	err = repo.Push(&git.PushOptions{
+		RemoteName: *remote,
 		RefSpecs: []config.RefSpec{
 			config.RefSpec("refs/tags/" + newTag + ":refs/tags/" + newTag),
 		},
 	})
-
 	if err != nil && err != git.NoErrAlreadyUpToDate {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Tag pushed 🚀")
+	fmt.Println("Tag pushed")
+}
+
+func resolveBumpMode(major bool, minor bool, patch bool) bumpMode {
+	enabled := 0
+	if major {
+		enabled++
+	}
+	if minor {
+		enabled++
+	}
+	if patch {
+		enabled++
+	}
+	if enabled > 1 {
+		log.Fatal("only one of --major, --minor, --patch can be set")
+	}
+
+	if major {
+		return bumpMajor
+	}
+	if minor {
+		return bumpMinor
+	}
+	return bumpPatch
+}
+
+func latestSemverTag(repo *git.Repository) (version, error) {
+	iter, err := repo.Tags()
+	if err != nil {
+		return version{}, err
+	}
+
+	latest := version{}
+	found := false
+
+	err = iter.ForEach(func(ref *plumbing.Reference) error {
+		tag := ref.Name().Short()
+		v, ok := parseSemverTag(tag)
+		if !ok {
+			return nil
+		}
+		if !found || latest.Less(v) {
+			latest = v
+			found = true
+		}
+		return nil
+	})
+	if err != nil {
+		return version{}, err
+	}
+
+	if !found {
+		return version{major: 0, minor: 0, patch: 0}, nil
+	}
+	return latest, nil
+}
+
+func parseSemverTag(tag string) (version, bool) {
+	match := semverTag.FindStringSubmatch(tag)
+	if match == nil {
+		return version{}, false
+	}
+
+	major, err := strconv.Atoi(match[1])
+	if err != nil {
+		return version{}, false
+	}
+	minor, err := strconv.Atoi(match[2])
+	if err != nil {
+		return version{}, false
+	}
+	patch, err := strconv.Atoi(match[3])
+	if err != nil {
+		return version{}, false
+	}
+
+	return version{
+		major: major,
+		minor: minor,
+		patch: patch,
+	}, true
+}
+
+func bump(v version, mode bumpMode) version {
+	switch mode {
+	case bumpMajor:
+		return version{major: v.major + 1}
+	case bumpMinor:
+		return version{major: v.major, minor: v.minor + 1}
+	default:
+		return version{major: v.major, minor: v.minor, patch: v.patch + 1}
+	}
 }
